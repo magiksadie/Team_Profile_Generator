b54aa6e964616e343cc741e606f405bca7a59175
sadie.harty <82483760+magiksadie@users.noreply.github.com>
2022-05-15 14:40:59 -0500

    refactored styling with bootstrap

--- src/page.template.js
+++ src/page.template.js
@@ -1,67 +1,105 @@
-function generateHTML(team) {
+const generateProfile = (employeeData) => {
+    console.log({ employeeData });
+
+    const pageHtml = [];
+
+    const generateManager = (manager) => {
+
+    let managerCard = `
+    <div class="card-columns flex-md-column">
+    <div class="card shadow-lg p-3 mb-5 bg-white rounded" style="width: 18rem;">
+        <div class="card-body bg-primary">
+            <h5 class="card-title text-light font-weight-bold"> ${manager.name} </h5>
+            <p class="card-text text-light"><i class="fa-solid fa-mug-hot"></i> Manager</p>
+        </div>
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">ID: ${manager.id} </li>
+            <li class="list-group-item">Email: <a href="mailto:${manager.email}"> ${manager.email}</a></li>
+            <li class="list-group-item">Office Number: ${manager.officeNumber} </li>
+        </ul>
+    </div>`;
+    pageHtml.push(managerCard);
+  };
+
+  const generateEngineer = (engineer) => {
+
+    let engineerCard = `
+        <div class="card shadow-lg p-3 mb-5 bg-white rounded" style="width: 18rem;">
+        <div class="card-body bg-primary">
+          <h5 class="card-title text-light font-weight-bold"> ${engineer.name} </h5>
+          <p class="card-text text-light"><i class="fa-solid fa-glasses"></i>  Engineer</p>
+        </div>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">ID: ${engineer.id} </li>
+          <li class="list-group-item">Email: <a href="mailto:${engineer.email}"> ${engineer.email}</a></li>
+          <li class="list-group-item">Github: <a href="https://github.com/${engineer.github}"> ${engineer.github} </a></li>
+        </ul>
+      </div>`;
+    pageHtml.push(engineerCard);
+  };
+
+  const generateIntern = (intern) => {
+
+
+    let internCard = `<div class="card shadow-lg p-3 mb-5 bg-white rounded" style="width: 18rem;">
+        <div class="card-body bg-primary">
+          <h5 class="card-title text-light font-weight-bold"> ${intern.name} </h5>
+          <p class="card-text text-light"><i class="fa-solid fa-user-graduate"></i> Intern</p>
+        </div>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">ID: ${intern.id} </li>
+          <li class="list-group-item">Email: <a href="mailto:${intern.email}"> ${intern.email}</a></li>
+          <li class="list-group-item">School: ${intern.school} </li>
+        </ul>
+      </div>`;
+    pageHtml.push(internCard);
+  };
+
+  for (let i = 0; i < employeeData.length; i++) {
+    if (employeeData[i].getRole() === 'Manager') {
+      generateManager(employeeData[i]);
+    }
+    if (employeeData[i].getRole() === 'Engineer') {
+      generateEngineer(employeeData[i]);
+    }
+    if (employeeData[i].getRole() === 'Intern') {
+      generateIntern(employeeData[i]);
+    }
+  }
+
+  return pageHtml.join('');
+};
+
+function generateHTML(employeeData) {
     return `
-    <!DOCTYPE html>
+        <!DOCTYPE html>
     <html lang="en">
     <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-        <title>Team Profile</title>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <title>Team Profile Generator</title>
+        <link rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+        crossorigin="anonymous"
+        />
+        <script src="https://kit.fontawesome.com/b2d82626e1.js" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="style.css" />
     </head>
     <body>
+        <div class="jumbotron jumbotron-fluid bg-danger">
         <div class="container">
-            <div class="row">
-                <div class="col-12">
-                    <div class="card-deck">
-                        ${team}
-                    </div>
-                </div>
-            </div>
+            <h1 class="display-4 text-center font-weight-bold text-light">My Team</h1>
+            <p class="lead"></p>
         </div>
+        </div>
+    <main> 
+    ${generateProfile(employeeData)}
+    </main>
     </body>
-    </html>
-    `;
-};
+</html>
+`;
+}
 
-const generateManager = (Manager) => {
-    let managerCard = `
-    <div class="card">
-        <div class="card-header">
-            <h2 class="card-title">${Manager.name}</h2>
-            <h3 class="card-title">${Manager.id}</h3>
-            <h3 class="card-title">${Manager.email}</h3>
-            <h3 class="card-title">${Manager.officeNumber}</h3>
-            </div>
-            </div>
-            `;
-    return managerCard;
-};
-
-const generateEngineer = (Engineer) => {
-    let engineerCard = `
-    <div class="card">
-        <div class="card-header">
-            <h2 class="card-title">${Engineer.name}</h2>
-            <h3 class="card-title">${Engineer.id}</h3>
-            <h3 class="card-title">${Engineer.email}</h3>
-            <h3 class="card-title">${Engineer.github}</h3>
-            </div>
-            </div>
-            `;
-    return engineerCard;
-};
-
-const generateIntern = (Intern) => {
-    let internCard = `
-    <div class="card">
-        <div class="card-header">
-            <h2 class="card-title">${Intern.name}</h2>
-            <h3 class="card-title">${Intern.id}</h3>
-            <h3 class="card-title">${Intern.email}</h3>
-            <h3 class="card-title">${Intern.school}</h3>
-            </div>
-            </div>
-            `;
-    return internCard;
-};
+module.exports = {generateHTML};
